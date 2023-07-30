88bc220642ed6e9674ce09d10f0ae842b5311bac
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-07-30 12:50:56 -0400

    create mysql package

---
+++ internal/infra/mysql/mysql.go
@@ -0,0 +1,28 @@
+package mysql
+
+import (
+	"database/sql"
+
+	"github.com/i3onilha/MESEnterpriseSmart/config"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/infra/mysql/labels"
+)
+
+type MySQL struct {
+	Labels *labels.Queries
+}
+
+func New() (*MySQL, error) {
+	c, err := config.New()
+	if err != nil {
+		return nil, err
+	}
+	db := c.GetDB()
+	conn, err := sql.Open(db.GetDriver(), db.GetDataSourceName())
+	if err != nil {
+		return nil, err
+	}
+	labels := labels.New(conn)
+	return &MySQL{
+		Labels: labels,
+	}, nil
+}
