9d92bdbd5d21c797d5492378f825e659cb4bb4de
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-07-31 22:09:00 -0400

    update MySQL queries

--- databases/sql/mysql/queries/labels.sql
+++ databases/sql/mysql/queries/labels.sql
@@ -1,20 +1,29 @@
 -- name: CreateLabel :execresult
-INSERT IGNORE INTO labels (uuid) VALUES (uuid());
+INSERT IGNORE INTO labels () VALUES ();
 
 -- name: GetLabel :one
 SELECT
   labels_data.*
 FROM
   labels_data
-  INNER JOIN labels ON labels_data.uuid = labels.uuid
-  LEFT JOIN labels_deletes on labels.uuid = labels_deletes.uuid
-WHERE labels_deletes.uuid IS NULL
+  LEFT JOIN labels_deletes ON labels_data.id = labels_deletes.id
+WHERE labels_deletes.id IS NULL AND labels_data.id = ?
 ORDER BY labels_data.created_at DESC
 LIMIT 1;
 
+-- name: GetLabelList :many
+SELECT
+  labels_data.*
+FROM
+  labels_data
+  LEFT JOIN labels_deletes ON labels_data.id = labels_deletes.id
+WHERE labels_deletes.id IS NULL
+ORDER BY labels_data.created_at DESC
+LIMIT ? OFFSET ?;
+
 -- name: UpdateLabel :execresult
-INSERT INTO labels_data (uuid, customer, family, model, part_number, station, label)
+INSERT INTO labels_data (id, customer, family, model, part_number, station, label)
   VALUES(?, ?, ?, ?, ?, ?, ?);
 
 -- name: DeleteLabel :exec
-INSERT INTO labels_deletes (uuid) VALUES (?);
+INSERT INTO labels_deletes (id) VALUES (?);
