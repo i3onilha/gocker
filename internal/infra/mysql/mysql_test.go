6b83322f65ee59befb9d37c2d84c88bc4f0f4351
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-08-05 11:19:56 -0400

    create MySQL test

---
+++ internal/infra/mysql/mysql_test.go
@@ -0,0 +1,32 @@
+package mysql_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/i3onilha/MESEnterpriseSmart/config"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/infra/mysql"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/infra/mysql/labels"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	c, err := config.New()
+	assert.Nil(t, err)
+	dataSourceName := c.GetDB().GetDataSourceName()
+	db, err := mysql.New(dataSourceName)
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+	ctx := context.Background()
+	params := labels.CreateLabelParams{
+		Customer:   "customer mysql",
+		Family:     "family",
+		Model:      "model created",
+		PartNumber: "partnumber 2",
+		Station:    "station created",
+		Label:      "label created",
+		Author:     "bc0g8101",
+	}
+	db.Labels.CreateAndUpdateLabel(ctx, dataSourceName, params)
+}
