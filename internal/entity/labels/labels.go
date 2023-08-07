787ac02ab35762f71bb9c3590a81d0fda751e112
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-08-07 16:47:55 -0400

    add validate tags

--- internal/entity/labels/labels.go
+++ internal/entity/labels/labels.go
@@ -3,24 +3,24 @@ package labels
 import "time"
 
 type CreateDTO struct {
-	ID         int32
-	Customer   string
-	Family     string
-	Model      string
-	PartNumber string
-	Station    string
-	Label      string
-	Author     string
+	ID         int32  `validate:"gte=0"`
+	Customer   string `validate:"required,max=32"`
+	Family     string `validate:"required,max=8"`
+	Model      string `validate:"required,max=16"`
+	PartNumber string `validate:"required,max=16"`
+	Station    string `validate:"required,max=32"`
+	Label      string `validate:"required,min=6,max=65535"`
+	Author     string `validate:"required"`
 	CreatedAt  time.Time
 }
 
 type UpdateDTO struct {
-	ID         int32
-	Customer   string
-	Family     string
-	Model      string
-	PartNumber string
-	Station    string
-	Label      string
-	Author     string
+	ID         int32  `validate:"gte=0"`
+	Customer   string `validate:"required,max=32"`
+	Family     string `validate:"required,max=8"`
+	Model      string `validate:"required,max=16"`
+	PartNumber string `validate:"required,max=16"`
+	Station    string `validate:"required,max=32"`
+	Label      string `validate:"required,min=6,max=65535"`
+	Author     string `validate:"required"`
 }
