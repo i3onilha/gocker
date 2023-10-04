d2f0b1db95f6eeb73c5361875423e1994987a1f0
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-10-04 09:54:55 -0400

    create  controls

---
+++ internal/control/labels/labels.go
@@ -0,0 +1,338 @@
+package labels
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/i3onilha/MESEnterpriseSmart/config"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/entity/labels"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/infra/mysql"
+	repository "github.com/i3onilha/MESEnterpriseSmart/internal/infra/repository/labels"
+	validator "github.com/i3onilha/MESEnterpriseSmart/internal/infra/validator/labels"
+	usecase "github.com/i3onilha/MESEnterpriseSmart/internal/usecase/labels"
+)
+
+type RepSQL struct {
+	REP_QUERY string `json:"REP_QUERY"`
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var createLabelDTO labels.CreateLabelDTO
+	session := chi.URLParam(r, "session")
+	err := json.NewDecoder(r.Body).Decode(&createLabelDTO)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// extract this to a function start
+	SQLs := make(map[string]string)
+	for _, setup := range createLabelDTO.Setup {
+		reportID := setup.ReportID
+		data := url.Values{
+			"funcao":       {"CDQCREPORTSQLT::getSQLReport"},
+			"conn":         {"padb"},
+			"dados[d][id]": {reportID},
+		}
+		url := "http://10.58.64.198:8081/workshop/webservice.php?session=" + session //#TODO: change to config
+		resp, err := http.PostForm(url, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var repSQL RepSQL
+		err = json.Unmarshal(body, &repSQL)
+		if err != nil {
+			msg := "please check if report id " + reportID + " exists: "
+			http.Error(w, msg+err.Error(), http.StatusBadRequest)
+			return
+		}
+		key := setup.ReportID + "_" + setup.ReportName
+		value := repSQL.REP_QUERY
+		if setup.Start != "" {
+			value = strings.Replace(value, "':START'", setup.Start, -1)
+		}
+		if setup.X != "" {
+			value = strings.Replace(value, "':X'", setup.X, -1)
+		}
+		SQLs[key] = value
+	}
+	sqlQueries, err := json.Marshal(SQLs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	createDto := &labels.CreateDTO{
+		Customer:   createLabelDTO.Customer,
+		Model:      createLabelDTO.Model,
+		PartNumber: createLabelDTO.PartNumber,
+		Station:    createLabelDTO.Station,
+		Dpi:        createLabelDTO.Dpi,
+		Label:      createLabelDTO.Label,
+		Setup:      createLabelDTO.Setup,
+		Author:     createLabelDTO.Author,
+		SqlQueries: string(sqlQueries),
+	}
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	created, err := usec.Create(createDto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// extract this to a function and
+	id := strconv.Itoa(int(created.ID))
+	resp := map[string]string{
+		"status":  "OK",
+		"message": "Label created successfully",
+		"id":      id,
+	}
+	buf, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(buf)
+}
+
+func ListByParts(w http.ResponseWriter, r *http.Request) {
+	partNumber := chi.URLParam(r, "part_number")
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	list, err := usec.ListByParts(partNumber)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	buf, err := json.Marshal(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(buf)
+}
+
+func ListByPartsAndStationAndDpi(w http.ResponseWriter, r *http.Request) {
+	partNumber := chi.URLParam(r, "part_number")
+	station := chi.URLParam(r, "station")
+	dpi := chi.URLParam(r, "dpi")
+	dpiNumber, err := strconv.Atoi(dpi)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	list, err := usec.ListByPartsAndStationAndDpi(partNumber, station, dpiNumber)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	buf, err := json.Marshal(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(buf)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	var updateLabelDTO labels.UpdateLabelDTO
+	session := chi.URLParam(r, "session")
+	err := json.NewDecoder(r.Body).Decode(&updateLabelDTO)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// extract this to a function start
+	id, err := strconv.Atoi(updateLabelDTO.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	SQLs := make(map[string]string)
+	for _, setup := range updateLabelDTO.Setup {
+		reportID := setup.ReportID
+		data := url.Values{
+			"funcao":       {"CDQCREPORTSQLT::getSQLReport"},
+			"conn":         {"padb"},
+			"dados[d][id]": {reportID},
+		}
+		url := "http://10.58.64.198:8081/workshop/webservice.php?session=" + session //#TODO: change to config
+		resp, err := http.PostForm(url, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		var repSQL RepSQL
+		err = json.Unmarshal(body, &repSQL)
+		if err != nil {
+			msg := "please check if report id " + reportID + " exists: "
+			http.Error(w, msg+err.Error(), http.StatusBadRequest)
+			return
+		}
+		key := setup.ReportID + "_" + setup.ReportName
+		value := repSQL.REP_QUERY
+		if setup.Start != "" {
+			value = strings.Replace(value, "':START'", setup.Start, -1)
+		}
+		if setup.X != "" {
+			value = strings.Replace(value, "':X'", setup.X, -1)
+		}
+		SQLs[key] = value
+	}
+	sqlQueries, err := json.Marshal(SQLs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	updateDto := &labels.UpdateDTO{
+		ID:         int32(id),
+		Customer:   updateLabelDTO.Customer,
+		Model:      updateLabelDTO.Model,
+		PartNumber: updateLabelDTO.PartNumber,
+		Station:    updateLabelDTO.Station,
+		Dpi:        updateLabelDTO.Dpi,
+		Label:      updateLabelDTO.Label,
+		Setup:      updateLabelDTO.Setup,
+		Author:     updateLabelDTO.Author,
+		SqlQueries: string(sqlQueries),
+	}
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	_, err = usec.Update(updateDto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// extract this to a function end
+	resp := map[string]string{
+		"status":  "OK",
+		"message": "Label updated successfully",
+		"id":      updateLabelDTO.ID,
+	}
+	buf, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(buf)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	idNum, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = usec.DeleteByID(idNum)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	resp := map[string]string{
+		"status":  "OK",
+		"message": "Label deleted successfully",
+		"id":      id,
+	}
+	buf, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Write(buf)
+}
---
+++ internal/control/zpl/zpl.go
@@ -0,0 +1,157 @@
+package zpl
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/godror/godror" // v0.35.1
+	"github.com/i3onilha/MESEnterpriseSmart/config"
+	"github.com/i3onilha/MESEnterpriseSmart/internal/infra/mysql"
+	repository "github.com/i3onilha/MESEnterpriseSmart/internal/infra/repository/labels"
+	validator "github.com/i3onilha/MESEnterpriseSmart/internal/infra/validator/labels"
+	usecase "github.com/i3onilha/MESEnterpriseSmart/internal/usecase/labels"
+)
+
+type RepLabel struct {
+	Label string                   `json:"label"`
+	Data  []map[string]interface{} `json:"data"`
+}
+
+func GetZPLCode(w http.ResponseWriter, r *http.Request) {
+	partNumber := chi.URLParam(r, "part_number")
+	station := chi.URLParam(r, "station")
+	dpi := chi.URLParam(r, "dpi")
+	keyReplace := chi.URLParam(r, "key")
+	dpiNumber, err := strconv.Atoi(dpi)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	c, err := config.New()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	queries, err := mysql.New(c.GetDB().GetDataSourceName())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repo := repository.New(queries)
+	vali := validator.New()
+	usec := usecase.New(repo, vali)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	list, err := usec.ListZPLByPartsAndStationAndDpi(partNumber, station, dpiNumber)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	repLabels := []RepLabel{}
+	for _, label := range list {
+		repLabel := RepLabel{
+			Label: label.Label,
+		}
+		sqlQueries := make(map[string]string)
+		err := json.Unmarshal([]byte(label.SqlQueries), &sqlQueries)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		for key, sqlQuery := range sqlQueries {
+			var loopVar bool
+			columns := []string{}
+			for _, set := range label.Setup {
+				if set.ReportID+"_"+set.ReportName == key {
+					columns = append(columns, set.Variable)
+					loopVar = set.LoopVar
+				}
+			}
+			d, err := execQuery(sqlQuery, keyReplace, chi.URLParam(r, "serial"), loopVar)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			if loopVar {
+				column := strings.Join(columns, "")
+				column = strings.TrimPrefix(column, "{{ ")
+				column = strings.TrimSuffix(column, " }}")
+				column = strings.TrimPrefix(column, "{{")
+				column = strings.TrimSuffix(column, "}}")
+				d = fmt.Sprintf(`{"%s":%s}`, column, d)
+			}
+			var data map[string]interface{}
+			err = json.Unmarshal([]byte(d), &data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			repLabel.Data = append(repLabel.Data, data)
+		}
+		repLabels = append(repLabels, repLabel)
+	}
+	json.NewEncoder(w).Encode(repLabels)
+}
+
+func execQuery(sqlQuery, key, value string, loopVar bool) (string, error) {
+	sqlQuery = strings.ReplaceAll(sqlQuery, fmt.Sprintf(":%s", key), value)
+	// extract to config start
+	db, err := sql.Open("godror", `user="tmcp" password="padboratmcp" connectString="10.57.64.131:1521/PADB"`)
+	if err != nil {
+		return "", err
+	}
+	defer db.Close()
+	// extract to config end
+	rows, err := db.Query(sqlQuery)
+	defer rows.Close()
+	if err != nil {
+		return "", err
+	}
+	jsonStr, err := jsonSerialize(rows)
+	if err != nil {
+		return "", err
+	}
+	if loopVar {
+		jsonStr = fmt.Sprintf("[%s]", jsonStr)
+	}
+	return jsonStr, nil
+}
+
+func jsonSerialize(rows *sql.Rows) (string, error) {
+	colNames, err := rows.Columns()
+	if err != nil {
+		return "", err
+	}
+	cols := make([]interface{}, len(colNames))
+	colPtrs := make([]interface{}, len(colNames))
+	for i := 0; i < len(colNames); i++ {
+		colPtrs[i] = &cols[i]
+	}
+	var result = make(map[string]interface{})
+	var jsonStr string
+	for rows.Next() {
+		err = rows.Scan(colPtrs...)
+		if err != nil {
+			return "", err
+		}
+		for i, col := range cols {
+			result[colNames[i]] = col
+		}
+		jsonStr += "{"
+		for key, val := range result {
+			jsonStr += fmt.Sprintf(`"%s":"%s",`, key, val)
+		}
+		jsonStr = strings.TrimSuffix(jsonStr, ",")
+		jsonStr += "},"
+	}
+	jsonStr = strings.TrimSuffix(jsonStr, ",")
+	return jsonStr, nil
+}
