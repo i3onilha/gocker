36a77be18b04bbe38852253a89b0b9684ca1e297
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-10-09 11:27:29 -0400

    remove required to update partnumber

--- internal/entity/labels/labels.go
+++ internal/entity/labels/labels.go
@@ -57,7 +57,7 @@ type UpdateDTO struct {
 	ID         int32  `validate:"gte=0"`
 	Customer   string `validate:"required,max=32"`
 	Model      string `validate:"required,max=16"`
-	PartNumber string `validate:"required,max=16"`
+	PartNumber string `validate:"max=16"`
 	Station    string `validate:"required,max=32"`
 	Dpi        int32  `validate:"required,gte=0,lte=600"`
 	Label      string `validate:"required,min=6,max=65535"`
