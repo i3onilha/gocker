0e818ad73e4f0f7c7d7588d87651b47f9a6212bc
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-11-12 19:20:50 -0400

    change app title

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "MESStationAssistant",
+		Title:  "MES Station Assistant",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
