e820bd62500c6a3a7c61b5eeb06b5f06c319114c
Jean Bonilha <jeanbonilha.webdev@gmail.com>
2023-11-16 09:54:01 -0400

    create Settings clear page

---
+++ frontend/src/content/dashboards/Settings/index.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from 'react-helmet-async';
+import { Container, Grid } from '@mui/material';
+import Footer from 'src/components/Footer';
+
+function DashboardCrypto() {
+  return (
+    <>
+      <Helmet>
+        <title>Settings Dashboard</title>
+      </Helmet>
+      <Container maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="stretch"
+          spacing={4}
+        >
+        </Grid>
+      </Container>
+      <Footer />
+    </>
+  );
+}
+
+export default DashboardCrypto;
